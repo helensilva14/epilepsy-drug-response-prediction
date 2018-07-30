1c85aeed18cddc2dd5eeb5379b5609546f1d04a1
Helen Silva <helen.silva@eldorado.org.br>
2018-07-30 14:12:43 -0300

    Updated notebook to evaluate genetic data (v4): fixed ROC comparison generation

---
+++ check_versions.py
@@ -0,0 +1,14 @@
+import platform
+import pandas as pd
+import numpy as np
+import sklearn as sk
+import matplotlib
+
+print(platform.sys.version)
+
+print(pd.__version__)
+print(np.__version__)
+print(sk.__version__)
+print(matplotlib.__version__)
+
+
