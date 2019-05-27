dd6eff7c63a4a6d8a72e8c088c990ebad6a3a290
Helen Silva <helensilva14@gmail.com>
2019-05-27 02:43:48 +0000

    Consolidated ECDF methods

--- base/plot_graphs.py
+++ base/plot_graphs.py
@@ -1,10 +1,10 @@
-import numpy as np
 import itertools
-
+import numpy as np
 import matplotlib.pyplot as plt
 
-# Function to plot Confusion Matrix without Normalization
 def plot_confusion_matrix(cm, model_name, file_name):
+    """Function to plot a confusion matrix without normalization"""
+    
     plt.clf()
     plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Wistia)
     plt.title('Drug Response Prediction Confusion Matrix without Normalization')
@@ -14,19 +14,23 @@ def plot_confusion_matrix(cm, model_name, file_name):
     tick_marks = np.arange(len(classNames))
     plt.xticks(tick_marks, classNames, rotation=45)
     plt.yticks(tick_marks, classNames)
+    
     # set the layout approach without normalization
     s = [['TN','FP'], ['FN', 'TP']]
     for i in range(2):
         for j in range(2):
             plt.text(j, i, str(s[i][j])+" = "+str(cm[i][j]))
+            
     # save plot as image 
     plt.savefig('../figures/default-confusion-matrixes/%s-%s-default-cm' % (model_name.lower(), file_name))
     plt.show()    
 
-# Function to plot a Normalized Confusion Matrix
 def plot_normalized_confusion_matrix(cm, model_name, file_name, use_portuguese=False):
+    """Function to plot a normalized confusion matrix"""
+    
     # apply normalization
     cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    
     # plot matrix
     plt.clf()
     plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
@@ -48,6 +52,7 @@ def plot_normalized_confusion_matrix(cm, model_name, file_name, use_portuguese=F
     tick_marks = np.arange(len(classNames))
     plt.xticks(tick_marks, classNames, rotation=45)
     plt.yticks(tick_marks, classNames)
+    
     # set the normalized layout approach
     fmt = '.2f' 
     thresh = cm.max() / 2.
@@ -55,13 +60,15 @@ def plot_normalized_confusion_matrix(cm, model_name, file_name, use_portuguese=F
         plt.text(j, i, format(cm[i, j], fmt), 
                  horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
     plt.tight_layout()
+    
     # save plot as image 
     plt.savefig('../figures/normalized-confusion-matrixes/%s-%s-normalized-cm.pdf' % (model_name.lower(), file_name), dpi=300, 
                 pad_inches=0, bbox_inches='tight')
     plt.show()
 
-# Function to plot a ROC curve 
 def plot_roc_curve(fpr, tpr, auc_score, model_name, file_name, use_portuguese=False):
+    """Generic function to plot a ROC curve"""
+    
     plt.figure(1)
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
@@ -81,4 +88,29 @@ def plot_roc_curve(fpr, tpr, auc_score, model_name, file_name, use_portuguese=Fa
     plt.legend(loc='best')
     # save plot as image 
     plt.savefig('../figures/roc-curves/%s-%s-roc-curve.pdf' % (model_name.lower(), file_name), dpi=300)
+    plt.show()
+    
+def calculate_ecdf(predprobs):
+    """Function to calculate the ECDF (empirical cumulative distribution function) for prediction probabilities"""
+    
+    predprobs = np.sort(predprobs)
+    
+    percentiles = list()  
+    for i in np.arange(1, len(predprobs)+1):
+        percentiles.append(i/len(predprobs))
+        
+    return percentiles
+
+def plot_ecdf(predprobs_responsive, predprobs_refractory):
+    """Function to plot the ECDF values vs. Probability Predictions"""
+    
+    plt.figure(figsize=(10,8))
+    plt.plot(np.sort(predprobs_responsive), calculate_ecdf(predprobs_responsive), color='blue', label='Responsive')
+    plt.plot(np.sort(predprobs_refractory), calculate_ecdf(predprobs_refractory), color='red', label='Refractory')
+    plt.grid(True)
+    plt.xlabel('Prediction probabilities')
+    plt.ylabel('ECDF')
+    plt.title('ECDF for Drug Response Probability Predictions') 
+    plt.legend(loc='best')
+    plt.savefig('../figures/ecdf/loocv-best-model-genetic-data-ecdf.pdf', dpi=300)
     plt.show()
